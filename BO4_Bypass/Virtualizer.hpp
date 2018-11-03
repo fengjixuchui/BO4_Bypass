81a9c194ffdef40b8474eb21168cf622bfdf7c4f
Windy <19060@qq.com>
2018-11-03 12:31:43 +0800

    [+] Init

---
+++ BO4_Bypass/Virtualizer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#if(USE_VMPROTECT == 1)
+#include <VirtualizerSDK.h>
+#ifdef _WIN32 
+#pragma comment(lib,"COFF\\VirtualizerSDK32")
+#elif _WIN64
+#pragma comment(lib,"COFF\\VirtualizerSDK64")
+#endif
+#else
+static void VirtualizerStart() {}
+static void VirtualizerEnd() {}
+#endif
